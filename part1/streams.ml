6a78356978e0e8443ceb275ff0dd1239665182c5
Quinn Beightol <qeb2@cornell.edu>
2014-10-08 22:21:47 -0400

    revised streams for clarity

--- part1/streams.ml
+++ part1/streams.ml
@@ -2,52 +2,76 @@ type 'a stream = Stream of 'a * (unit -> 'a stream)
 
 let id           = fun x -> x
 let fork (f,g) x = f x, g x
+let comp f g x = f (g x)
+
+let foldl1 f f_hd l =
+  match l with
+  | hd::tl -> List.fold_right f (f_hd hd) tl
+  | _ -> failwith "empty list"
+
+(*returns a list containing the first n elements of a string*)
 let rec take n (Stream (h,t)) = if n=0 then [] else h::take (n-1) (t ())
 
-let rec unfold (f:  'a -> 'b * 'a) (s: 'a) : 'b stream = 
-    match f s with (hd, s') -> Stream (hd, fun () -> (unfold f s'))
+(*takes in a generating function, [f] and a seed and produces a stream
+  note: [f] takes in a seed and returns the next element in the stream, along
+  with a new seed for generating the rest of the stream*)
+let rec unfold (f:  'a -> 'b * 'a) (s: 'a) : 'b stream =
+  let (hd, s') = f s in Stream (hd, fun () -> unfold f s')
 
-let univ (f,g) x = (fun x -> unfold (fork x)) (f,g) x
+(*univ behaves like unfold only it takes a tuple of generators, where the first
+  generating function produces a new element using the current seed, and the
+  second produces the next seed*)
+let univ fs s = unfold (fork fs) s
 
 let hd (Stream (h,_)) = h
 let tl (Stream (_,t)) = t ()
 
-let repeat (x: 'a) : 'a stream = univ (id, id) x
+let rec nth n s =
+  if n < 0 then failwith "nth isn't defined for negative n"
+  else if n = 0 then hd s
+  else nth (n-1) (tl s)
 
-let map (f: 'a -> 'b) (s: 'a stream) : 'b stream = 
-    let f' = (fun x -> f (hd x)) in
-        match s with  Stream(h,t) -> univ (f', tl) s
+(*creates a stream consisting only of x*)
+let repeat x = univ (id, id) x
 
-let diag (s: 'a stream stream) : 'a stream = 
-    univ ((fun s' -> hd (hd s')), (fun s' -> map tl (tl s'))) s
+(*applies f (lazily) to each element of the stream*)
+let map f s = univ (comp f hd, tl) s
 
+(*returns the diagonal (as a stream) of a stream of streams; i.e. it returns
+  a stream whose ith element is the ith element from ith stream in the input *)
+let diag s = univ (comp hd hd, comp (map tl) tl) s
+
+(*takes in a stream s0 s1 s2 s3 ... and returns
+  s0  s1  s2  ...
+  s1  s2  s3  ...
+  s2  s3  s4  ...
+  ... ... ... ... *)
 let suffixes (s: 'a stream) : 'a stream stream = univ (id, tl) s
 
-let interleave (s: 'a stream) (s': 'a stream) : 'a stream =
-    univ ((fun (s1,_) -> hd s1), (fun (s1,s2) -> s2, (tl s1))) (s, s')
+(*takes in a stream s0 s1 s2 ... and another stream t0 t1 t2 ... and returns
+  s0 t0 s1 t1 s2 t2 ... *)
+let interleave s t = univ (comp hd fst, fun (s,t) -> t, tl s) (s,t)
 
-let fibs = univ ((fun (e, _) -> e), (fun (e1, e2) -> (e1+e2, e1))) (0,1)
+(*a stream of fibonacci numbers*)
+let fibs = univ (fst, fun (e, e') -> e + e', e) (0,1)
 
-
+(*calculates 1 to the nth power*)
 let one_to_n n = if (n mod 2 = 0) then 1. else -1.
 
 (*computes the nth term--but not partial sum--of  the summation identity for
    pi provided on page 5 of the ps4 handout.*)
 let nth_term n = 4. *. ((one_to_n n)/. (2. *. (float_of_int n) +. 1.))
 
-let pi = 
-    univ ((fun (s, n) -> s), (fun (s,n) -> (s +. nth_term n, succ n))) (4., 1)
+(*a stream whose nth element is nth partial sum of the formula for pi from the
+  handout*)
+let pi = univ (fst, fun (s,n) -> s +. nth_term n, succ n) (0.0, 0)
 
-let look (l: int list) : (int * int) list =
-    let look (e: int) (counts: (int * int) list) : (int * int) list =
-        (match counts with
-        | [] -> (1,e)::[]
-        | (hc, he)::t -> if (he = e) then (hc + 1, he)::t
-                                else (1, e)::counts) in
-    List.fold_right look l []
 
-let say (l: (int * int) list ) : int list =
-    let say ((c, e) : (int * int)) (old: int list) : int list = c::e::old in
-    List.fold_right say l []
+  let f (n, count, l) e = if e = n then (n, count + 1, l) else (e, 0, n::e::l)
+  in let (_, _, lst) = foldl1 f (fun hd -> [0;hd] l in lst
 
-let look_and_say = univ (id, (fun x -> say (look x))) [1]
+let look_n_say (l: int list) : int list =
+  let f (n::count::l) e = if e = n then n::(count + 1)::l else e::1::n::count::l
+  in List.rev (foldl1 f (fun hd -> [hd;1]) l)
+
+let look_and_say = univ (id, look_n_say) [1]
